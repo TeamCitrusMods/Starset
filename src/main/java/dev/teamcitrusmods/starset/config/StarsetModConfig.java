428c8589372892aef9be019d66d2a00885df5309
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2023-03-28 12:20:56 +0100

    v1.0.0

--- src/main/java/dev/teamcitrusmods/starset/config/StarsetModConfig.java
+++ src/main/java/dev/teamcitrusmods/starset/config/StarsetModConfig.java
@@ -21,19 +21,20 @@ public class StarsetModConfig {
 
     private static void setupConfig(ForgeConfigSpec.Builder builder) {
         builder.push("Player Ego Config");
-        badEgoEffectsMin = builder.comment("The value which defines the point in a player's ego where they start receiving bad effects").defineInRange("badEgoEffectsMin", 70, 1, 99);
+        badEgoEffectsMin = builder.comment("Minimum value for at which the player starts to receive Negative Ego effects.")
+                .defineInRange("badEgoEffectsMin", 70, 1, 99);
         builder.pop();
 
         builder.push("Etheria Chunk Config");
-        richEtheriaChunkChance = builder.comment("The chance for a rich etheria chunk to spawn. Value is a percent. E.g. 1 is 1% chance to spawn")
+        richEtheriaChunkChance = builder.comment("Chance for a Rich Etheria chunk to generate instead of a normal Etheria chunk. Value is a percentage. E.g. a value of 1 is 1%")
                 .defineInRange("richEtheriaChunkChance", 1, 1, 100);
-        normalEtheriaChunkMin = builder.comment("The value which defines the minimum value that the etheria in a chunk can naturally spawn at")
+        normalEtheriaChunkMin = builder.comment("Value that defines the minimum Etheria cap of a normal Etheria chunk can generate as.")
                 .defineInRange("normalEtheriaChunkMin", 550, 1, Integer.MAX_VALUE);
-        normalEtheriaChunkMax = builder.comment("The value which defines the maximum value that the etheria in a chunk can naturally spawn at. Cannot be smaller than 'normalEtheriaChunkMin'")
+        normalEtheriaChunkMax = builder.comment("Value that defines the maximum Etheria cap of a normal Etheria chunk can generate as. Cannot be smaller than 'normalEtheriaChunkMin'")
                 .defineInRange("normalEtheriaChunkMax", 650, 1, Integer.MAX_VALUE);
-        richEtheriaChunkMin = builder.comment("The value which defines the minimum value that etheria in a rich chunk can naturally spawn at")
+        richEtheriaChunkMin = builder.comment("Value that defines the minimum Etheria cap of a Rich Etheria chunk can generate as.")
                 .defineInRange("richEtheriaChunkMin", 900, 1, Integer.MAX_VALUE);
-        richEtheriaChunkMax = builder.comment("The value which defines the maximum value that etheria in a rich chunk can naturally spawn at. Cannot be smaller than 'richEtheriaChunkMin'")
+        richEtheriaChunkMax = builder.comment("Value that defines the maximum Etheria cap of a Rich Etheria chunk can generate as. Cannot be smaller than 'richEtheriaChunkMin'")
                 .defineInRange("richEtheriaChunkMax", 1000, 1, Integer.MAX_VALUE);
         builder.pop();
     }
